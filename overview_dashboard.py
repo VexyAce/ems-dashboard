7cf62695de4502c6089c72fe796d626d7a3b3d4b
Lim Wee <hgp.limwee@gmail.com>
2026-03-31 00:14:36 +0800

    Update

--- overview_dashboard.py
+++ overview_dashboard.py
@@ -166,7 +166,7 @@ def automated_daily_export():
 schedule.every(1).minutes.do(automated_daily_export)
 
 # 🔥 ADDED LINE (every 5 minutes)
-schedule.every(60).minutes.do(insert_energy_data)
+schedule.every(1).minutes.do(insert_energy_data)
 
 def run_scheduler():
     while True:
