7adf3d806b56dd5758e9ab701eb17e8c2ad6078b
EETC EMS <hgp.limwee@gmail.com>
2026-03-31 13:55:51 +0800

    Update

--- overview_dashboard.py
+++ overview_dashboard.py
@@ -24,6 +24,10 @@ engine = create_engine(
 
 CARBON_FACTOR = 0.408
 
+# =================================================
+# SYSTEM DEFINITIONS
+# =================================================
+
 systems = {
     "bss": {"label": "Boiler & Steam", "name": "Boiler & Steam System (BSS)", "scope": "Scope 1 – Direct Emissions"},
     "hps": {"label": "Heat Pump", "name": "Heat Pump System (HPS)", "scope": "Scope 2 – Electricity"},
